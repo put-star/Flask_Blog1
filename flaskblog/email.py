7278e259f9a07e29e2cc64d5cbd0c86013e816c3
put-star <putrialfiyyahd@gmail.com>
2020-11-02 19:01:36 +0700

    Project Pertama

---
+++ flaskblog/email.py
@@ -0,0 +1,12 @@
+import os
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+print(SECRET_KEY)
+print(SQLALCHEMY_DATABASE_URI )
+
+
+
+
